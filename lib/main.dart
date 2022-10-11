9462413292da0ea7d724faea13609290c266bc35
Brian Craig <briancraigok@gmail.com>
2022-10-11 20:11:50 -0300

    feat: remove sdk limitations

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/widgets.dart';
 
 void main() {
-  Map<String, String> env = Platform.environment;
-  env.forEach((k, v) => print("Key=$k Value=$v"));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Center(
